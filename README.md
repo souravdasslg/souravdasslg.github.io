527f8c6bfc26de90d48a7162ceaa1fef17c220ec
Sourav Das <sourav@edwisor.com>
2019-05-26 06:43:12 +0530

    Initial Release

---
+++ README.md
@@ -0,0 +1,1 @@
+# souravdasslg.github.io
---
+++ precache-manifest.c67057cf54e1264c8d093f73a53d16aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3eb19ebbb4e12ee8d73be4daae0a7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40bc9d5604da981ee59",
+    "url": "/static/css/main.ed698c4b.chunk.css"
+  },
+  {
+    "revision": "2d8ebececcd43c83549e",
+    "url": "/static/js/2.7672a1be.chunk.js"
+  },
+  {
+    "revision": "c40bc9d5604da981ee59",
+    "url": "/static/js/main.c94b87a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
